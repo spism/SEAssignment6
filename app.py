7c24f27907acf82519578e70ffe19ed3af862392
spism <m.spisak@student.maastrichtuniversity.nl>
2022-05-09 13:38:23 +0200

    Implemented new_order method

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
-from pizza import Pizza
+from order import Order
 from database import Database
+from datetime import datetime
 import json
 
 app = Flask(__name__)
 
 orders = Database("Orders")
 pizzas = Database("Pizzas")
+order_id = 0
 
 
 @app.route('/pizza')
@@ -25,8 +27,17 @@ def get_order(order_id: int):
 
 @app.route('/order', methods=["POST"])
 def new_order():
+    global order_id
+
     pizzas = request.form["pizzas"]
-    orders.add_object()
+    takeaway = request.form["takeaway"]
+    payment_type = request.form["payment_type"]
+    customer_id = request.form["customer_id"]
+    note = request.form["note"]
+    delivery_address = request.form["delivery_address"]
+    o = Order(order_id, customer_id, "ordered", datetime.now(), note, takeaway, payment_type, delivery_address, pizzas)
+    order_id += 1
+    orders.add_object(o)
 
 
 @app.route('/order/cancel/{order_id}', methods = ["PUT"])
