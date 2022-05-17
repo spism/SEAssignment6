bb44848b2c3c6bb04c0c98bbbb34cc61f370e841
lukaskr777 <76201682+lukaskr777@users.noreply.github.com>
2022-05-17 17:25:35 +0200

    final

--- app.py
+++ app.py
@@ -20,14 +20,6 @@ pizzas.add_object(p2)
 def home():
     return render_template("home.html")
 
-@app.route("/order/create")
-def create_order():
-    return render_template("create_order.html")
-
-@app.route("/order/display", methods = ['POST'])
-def display_order():
-    return make_response(request.form, 200)
-
 @app.route('/pizza')
 def get_pizzas():  # put application's code here
     return make_response({"list of pizzas": [pizza.to_dict() for pizza in pizzas.get_objects()]},200)
@@ -40,17 +32,22 @@ def get_pizza(pizza_id: int):
     if pizza is None:
         return make_response({"message": "pizza not found"},404)
     else:
-        return make_response(pizza.__dict__,200)
+        return make_response(pizza.to_dict(),200)
 
 
 @app.route('/order/<order_id>', methods = ["GET"])
 def get_order(order_id: int):
-    order = orders.get_object(int(order_id))
 
-    if order is None:
-        return make_response(json.dumps({"message": "order not found"}),404)
-    else:
-        return make_response(order.to_dict(),200)
+    try:
+        order = orders.get_object(int(order_id))
+
+        if order is None:
+            return make_response(json.dumps({"message": "Order_ID not found"}),404)
+        else:
+            return make_response(order.to_dict(),200)
+
+    except:
+        return make_response(json.dumps({"message": "Invalid ID supplied"}), 400)
 
 
 @app.route('/order', methods=["POST"])
@@ -60,12 +57,16 @@ def new_order():
     try:
         data = request.get_json()
 
-        pizza_list = [pizzas.get_object(pizza_id) for pizza_id in data["pizzas"]]
+        pizza_list = [pizzas.get_object(pizza_id)  for pizza_id in data["pizzas"]]
         takeaway = data["takeaway"]
         payment_type = data["payment_type"]
         customer_id = data["customer_id"]
         note = data["note"]
         delivery_address = data["delivery_address"]
+
+        # check the format of delivery address
+        if "street" not in delivery_address or "city" not in delivery_address or "country" not in delivery_address or "zipcode" not in delivery_address:
+            raise
 
         o = Order(order_id, customer_id, "In Progress", datetime.now(), note, takeaway, payment_type, delivery_address, pizza_list)
         order_id += 1
@@ -80,28 +81,29 @@ def new_order():
     except:
         return make_response({"message": "The format of the object is not valid"},400)
 
+
 @app.route('/order/cancel/<order_id>', methods = ["PUT"])
 def cancel_order(order_id: int):
     order = orders.get_object(int(order_id))
     if order is None:
-        return json.dumps({"message": "Order not found"})
+        return make_response({"message": "Order not found"},404)
     if order.status == "cancelled" or order.status == "delivered":
-        return json.dumps({"message": "Unable to cancel an already canceled or delivered order"})
+        return make_response({"message": "Unable to cancel an already canceled or delivered order"},422)
 
     diff = (datetime.now() - order.ordered_at).total_seconds()
     if diff > 300:
-        return json.dumps({"message": "Unable to cancel your order after 5 minutes have elapsed."})
+        return make_response({"message": "Unable to cancel your order after 5 minutes have elapsed."},412)
     else:
         order.status = "cancelled"
         orders.update_object(int(order_id),order)
-        return json.dumps({"order_id": order.get_id(), "status": "cancelled" })
+        return make_response({"order_id": order.get_id(), "status": "cancelled" },200)
 
 
 @app.route('/order/deliverytime/<order_id>', methods = ["GET"])
 def get_delivery_time(order_id : int):
     order = orders.get_object(int(order_id))
     if order is None:
-        return json.dumps({"message": "Order not found"})
+        return make_response({"message": "Order not found"},404)
     else:
         estimated_delivery_time = datetime.now()
         estimated_delivery_time = estimated_delivery_time.replace(hour=estimated_delivery_time.hour+ 1)
@@ -109,8 +111,19 @@ def get_delivery_time(order_id : int):
             "order": order.to_dict(),
             "delivery_time": estimated_delivery_time
         }
-        return json.dumps(d)
+        return make_response(d,200)
 
+@app.route('/order/delivered/<order_id>', methods = ["PUT"])
+def deliver_order(order_id: int):
+    order = orders.get_object(int(order_id))
+    if order is None:
+        return make_response({"message": "Order not found"},404)
+    if order.status == "cancelled" or order.status == "delivered":
+        return make_response({"message": "Unable to deliver an already canceled or delivered order"},422)
+
+    order.status = "delivered"
+    orders.update_object(int(order_id),order)
+    return make_response({"order_id": order.get_id(), "status": "delivered" },200)
 
 
 
